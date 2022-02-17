9b011e74c0dc3c98b94b5c971ee5771d57393250
Sreenath285 <sreenath.285@gmail.com>
2022-02-17 13:40:17 +0530

    finding largest among three numbers

---
+++ LargestAmongThreeNum/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num1, num2, num3;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any three numbers : ");
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        num3 = sc.nextInt();
+
+        if (num1 > num2 && num1 > num3) {
+            System.out.println(num1 + " is largest");
+        }
+        else if (num2 > num1 && num2 > num3) {
+            System.out.println(num2 + " is largest");
+        }
+        else {
+            System.out.println(num3 + " is largest");
+        }
+    }
+}
